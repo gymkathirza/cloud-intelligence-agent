40c0f1ae94719539a3b124f540b4b27e60aac9e4
gymkathirza <gymkathirza@gmail.com>
2026-04-27 00:35:40 +0000

    Terraform tweaks

--- infra/providers.tf
+++ infra/providers.tf
@@ -1,11 +1,5 @@
 terraform {
-  backend "s3" {
-    bucket         = "my-unique-tf-state-bucket"
-    key            = "dev/terraform.tfstate"
-    region         = "us-east-1"
-    encrypt        = true
-    dynamodb_table = "terraform-lock" # Prevents two people from running it at once
-  }
+  backend "s3" {}
 }
 
 provider "aws" {
