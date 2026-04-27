75b11c5505a743fc94af7bb150770bfcdbdf0813
gymkathirza <gymkathirza@gmail.com>
2026-04-27 02:03:04 +0000

    Updated Dynamo table to match the sampel data schema and requirement

--- infra/modules/dynamodb/main.tf
+++ infra/modules/dynamodb/main.tf
@@ -1,10 +1,18 @@
 resource "aws_dynamodb_table" "this" {
   name           = "${var.table_name}-${var.env}"
   billing_mode   = "PAY_PER_REQUEST"
-  hash_key       = "id"
+  
+  hash_key       = "ResourceId"
+  # Range_key (Sort Key)
+  range_key      = "ResourceType"
 
   attribute {
-    name = "id"
+    name = "ResourceId"
+    type = "S"
+  }
+
+  attribute {
+    name = "ResourceType"
     type = "S"
   }
 
